f727c596887b3eb50ff46419957ff2c5ac48c0ce
Alan Mir <alanpaulphotos@gmail.com>
2021-01-21 22:29:58 -0500

    Added userEvents and found a bug in email preventing me from grabbing the input rather than the label

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import ContactForm from "./ContactForm";
 
 test("renders ContactForm without crashing", () => {
@@ -14,5 +15,8 @@ test("recieves inputs, and submits", () => {
     const email = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
     const message = screen.getByRole('textbox');
 
-
+    userEvent.type(firstName, "Al");
+    userEvent.type(lastName, "Gertrude");
+    userEvent.type(email, "idk@wat.com");
+    userEvent.type(message, "I don't remember where I parked my car");
 });
